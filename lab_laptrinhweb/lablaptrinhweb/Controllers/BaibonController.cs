60e468b1e48306f4d758df9dc8a3936534018416
ambrouse <thichthichich3568gmail.com>
2022-11-23 11:21:49 +0700

    update-23-11-2023

---
+++ lab_laptrinhweb/lablaptrinhweb/Controllers/BaibonController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lablaptrinhweb.Controllers
+{
+    public class BaibonController : Controller
+    {
+        // GET: Baibon
+        public ActionResult Index()
+        {
+            return View();
+        }public ActionResult Login()
+        {
+            return View();
+        }public ActionResult Shop()
+        {
+            return View();
+        }public ActionResult cart()
+        {
+            return View();
+        }public ActionResult checkout()
+        {
+            return View();
+        }public ActionResult productdetails()
+        {
+            return View();
+        }public ActionResult blocgsingle()
+        {
+            return View();
+        }public ActionResult blocg()
+        {
+            return View();
+        }public ActionResult contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ lab_laptrinhweb/lablaptrinhweb/Controllers/CaculatorController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lablaptrinhweb.Models;
+
+namespace lablaptrinhweb.Controllers
+{
+    public class CaculatorController : Controller
+    {
+        // GET: Caculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Char dau,int soa,int sob)
+        {
+            if (dau == '+') {
+                ViewBag.kq = soa + sob;
+            }if (dau == '-') {
+                ViewBag.kq = soa - sob;
+            }
+            if (dau == '*') {
+                ViewBag.kq = soa * sob;
+            }
+            if (dau == '/') {
+                ViewBag.kq = soa / sob;
+            }
+            ViewBag.soa = soa;
+            ViewBag.sob = sob;
+            return View();
+        }
+    }
+}
